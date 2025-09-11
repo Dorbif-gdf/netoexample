cbd3fca3731c91c75f0924d20a330335c5df5695
Dorbif-gdf <kitropllay@gmail.com>
2025-09-11 18:10:35 +0300

    Add files via upload

---
+++ Rep_Word.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string str; 
+
+    std::cout << "Введите слово: ";
+    std::cin >> str;
+    std::cout << str;
+}
---
+++ Rep_num.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    int num; 
+
+    std::cout << "Введите любое число: ";
+    std::cin >> num; 
+    std::cout << num; 
+}

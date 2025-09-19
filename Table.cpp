891157e8c1cd40bdce6a1c2227fb3a60c4b7fd3a
Dorbif-gdf <kitropllay@gmail.com>
2025-09-19 22:13:35 +0300

    Add files via upload

---
+++ Table.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    bool A, B;
+
+    cout << "Оператор: ||\n";
+
+    A = true;  B = true;
+    cout << boolalpha << A << "\t" << B << "\t" << (A || B) << endl;
+
+    A = false;  B = true;
+    cout << boolalpha << A << "\t" << B << "\t" << (A || B) << endl;
+
+    A = true; B = false;
+    cout << boolalpha << A << "\t" << B << "\t" << (A || B) << endl;
+
+    A = false; B = false;
+    cout << boolalpha << A << "\t" << B << "\t" << (A || B) << endl;
+
+
+    cout << "\nОператор: &&\n";
+
+    A = true;  B = true;
+    cout << boolalpha << A << "\t" << B << "\t" << (A && B) << endl;
+
+    A = false;  B = true;
+    cout << boolalpha << A << "\t" << B << "\t" << (A && B) << endl;
+
+    A = true; B = false;
+    cout << boolalpha << A << "\t" << B << "\t" << (A && B) << endl;
+
+    A = false; B = false;
+    cout << boolalpha << A << "\t" << B << "\t" << (A && B) << endl;
+}

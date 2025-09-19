2edd7dbd939486f619a57d97e8ff911bdd036517
Dorbif-gdf <kitropllay@gmail.com>
2025-09-20 00:46:11 +0300

    Add files via upload

---
+++ Streamline numbers.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    int a, b, c;
+
+    cout << "Введите 3 целых числа: ";
+    cin >> a >> b >> c;
+    int max = (a > b ? (a > c ? a : c) : (b > c ? b : c));
+    int min = (a < b ? (a < c ? a : c) : (b < c ? b : c));
+    int mid = a + b + c - max - min;
+    cout << max << " " << mid << " " << min;
+}

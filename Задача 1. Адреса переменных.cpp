72dc7371478fb6f34adbd3a61176edf2a439ea4d
Dorbif-gdf <kitropllay@gmail.com>
2025-10-11 20:59:02 +0300

    Add files via upload

---
+++ Задача 1. Адреса переменных.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 42;
+    short s = -15;
+    long l = 123456;
+    long long ll = 9876543210;
+    float f = 3.14f;
+    double d = -27.089;
+    long double ld = 12345.6789012345L;
+    bool b = true;
+
+    cout << "int: " << ' ' << &i << ' ' << sizeof(i) << endl; 
+    cout << "short: " << ' ' << &s << ' ' << sizeof(s) << endl;
+    cout << "long: " << ' ' << &l << ' ' << sizeof(l) << endl;
+    cout << "long long: " << ' ' << &ll << ' ' << sizeof(ll) << endl;
+    cout << "float: " << ' ' << &f << ' ' << sizeof(f) << endl;
+    cout << "double: " << ' ' << &d << ' ' << sizeof(d) << endl;
+    cout << "long double: " << ' ' << &ld << ' ' << sizeof(ld) << endl;
+    cout << "bool: " << ' ' << &b << ' ' << sizeof(b) << endl;
+}
---
+++ Задача 2. Обмен значениями.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std; 
+
+
+void swap(int& x, int& y) {
+	x = x + y;  
+	y = x - y;  
+	x = x - y;  
+}
+
+int main()
+{
+	int a = 5, b = 8;
+
+	cout << "a = " << a << ", b = " << b << std::endl;
+
+	swap(a, b); 
+
+	cout << "a = " << a << ", b = " << b << std::endl;
+
+
+}

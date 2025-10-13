0523f95801f4ae6c7c26a07444dad6ba2d69de33
Dorbif-gdf <kitropllay@gmail.com>
2025-10-13 15:49:43 +0300

    Add files via upload

---
+++ Задача 1. Считающая функция.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std; 
+
+void counting_function() {
+    static int counter = 0;  
+    counter++;                
+    cout << "Количество вызовов функции counting_function(): " << counter << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
---
+++ Задача 2. Одинаковые, но разные функции.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+namespace addition {
+    int calc(int x, int y) {
+        return x + y;
+    }
+}
+
+namespace Subtraction {
+    int calc(int x, int y) {
+        return x - y;
+    }
+}
+
+namespace multiplication {
+    int calc(int x, int y) {
+        return x * y;
+    }
+}
+
+namespace division {
+    int calc(int x, int y) {
+        return y != 0 ? x / y : 0; 
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int x = 6;
+    int y = 9;
+
+    cout << "x = " << x << ", y = " << y << endl;
+    cout << "Сложение: " << addition::calc(x, y) << endl;
+    cout << "Вычитание: " << Subtraction::calc(x, y) << endl;
+    cout << "Умножение: " << multiplication::calc(x, y) << endl;
+    cout << "Деление: " << division::calc(x, y) << endl;
+}

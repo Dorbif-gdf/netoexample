a2ce4ba2e949c11cccb93b68181f346df13d864e
Dorbif-gdf <kitropllay@gmail.com>
2025-10-16 16:12:35 +0300

    Add files via upload

---
+++ Задача 1. Вывод массива.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+void print(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int arr2[] = { 6, 5, 4, 8 };
+    int arr3[] = { 1, 4, 3, 7, 5 };
+
+    print(arr1, 9);
+    print(arr2, 4);
+    print(arr3, 5);
+
+}
---
+++ Задача 2. Снова swap.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std; 
+
+void swap(int* x, int* y) {
+    *x = *x + *y;
+    *y = *x - *y;
+    *x = *x - *y;
+}
+
+int main() {
+    int a = 5;
+    int b = 8;
+
+    cout << "a = " << a << ", b = " << b << endl;
+
+    swap(&a, &b); 
+
+    cout << "a = " << a << ", b = " << b << endl;
+
+}

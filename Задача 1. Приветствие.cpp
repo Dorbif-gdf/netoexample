220999fdcdfef0980798ba2dfbe93af279ae6f1d
Dorbif-gdf <kitropllay@gmail.com>
2025-10-20 17:38:02 +0300

    Add files via upload

---
+++ Задача 1. Приветствие.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251); 
+    SetConsoleCP(1251);   
+
+    string name, surname;
+
+    cout << "Введите имя: ";
+    cin >> name;
+
+    cout << "Введите фамилию: ";
+    cin >> surname;
+
+    string greeting = "Здравствуйте, " + name + " " + surname + "!";
+    cout << greeting << endl;
+}
---
+++ Задача 2. Угадайка.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    string secret = "малина"; 
+    string guess;
+
+    do {
+        cout << "Угадайте слово: ";
+        cin >> guess;
+
+        if (guess != secret) {
+            cout << "Неправильно" << endl;
+        }
+    } while (guess != secret);
+
+    cout << "Правильно! Вы победили! Загаданное слово — " << secret << endl;
+}
